4c8068346e2afd69e40bc1542b5f861ed5abbd18
ELOUIZ BADR <elouiz.badr@gmail.com>
2017-05-18 10:59:14 +0100

    Move static data from Views to Controllers!

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,35 @@
 /* "homeList" function */
 module.exports.homeList = function(req, res) {
-	res.render('locationsList', {title: 'Home'});
+    res.render('locationsList', {
+        title: 'Loc8r - find a place to work with wifi!',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi!'
+        },
+        locations: [
+	    {
+		name: 'Starcups',
+		address: '125 High Street, Reading, RG6 1PS',
+		rating: 3,
+		facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+		distance: '100m',
+	    },
+	    {
+		name: 'McDonald\'s',
+		address: '125 Mohamed V, Casablanca, Morocco',
+		rating: 4,
+		facilities: ['Ice Cream', 'Food', 'Premium wifi'],
+		distance: '250m',
+	    },
+	    {
+		name: 'Starbucks',
+		address: '125 High Street, Reading, RG6 1PS',
+		rating: 3,
+		facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+		distance: '300m',
+	    }
+	]
+    });
 };
 
 /* "locationInfo" function */
