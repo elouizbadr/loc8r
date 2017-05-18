5132b795204ee88d5afb2d0a98abacc34f439255
ELOUIZ BADR <elouiz.badr@gmail.com>
2017-05-18 11:43:44 +0100

    Continue moving static data to Controllers, and create shared Jade functions directory!

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -28,7 +28,8 @@ module.exports.homeList = function(req, res) {
 		facilities: ['Hot drinks', 'Food', 'Premium wifi'],
 		distance: '300m',
 	    }
-	]
+	],
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
     });
 };
 
