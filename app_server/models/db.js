2232c2f1a56ce755cfa7183517a1111c5004d530
ELOUIZ BADR <elouiz.badr@gmail.com>
2017-05-18 14:45:10 +0100

    Add MongoDB database connectivity using Mongoose!

---
+++ app_server/models/db.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+
+/* Connect to MongoDB */
+var dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI);
+
+/* Monitor MongoDB Connection */
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connecttion error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected!');
+});
+
+/* Closing Connection to MongoDB */
+var gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close( function () {
+    	console.log('Mongoose disconnected through ' + msg);
+	callback();
+    });
+};
+
+// Listening for different events
+/* Nodemon uses SIGUSR2 */
+process.once('SIGUSR2', function () {
+    gracefulShutdown('nodemon restart', function () {
+    	process.kill(process.pid, 'SIGUSR2');
+    })
+});
+
+/* CTRL-C in terminal uses SIGINT */
+process.once('SIGINT', function () {
+    gracefulShutdown('app termination', function () {
+    	process.exit(0);
+    })
+});
+
+/* Heroku uses SIGTERM */
+process.once('SIGTERM', function () {
+    gracefulShutdown('heroku app shutdown', function () {
+    	process.exit(0);
+    })
+});
